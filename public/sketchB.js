53c18224957c8004a35686c0c53779d4a7c6f809
JAMES SANDERS <jsanders@ca.ibm.com>
2017-01-25 10:53:03 -0600

    fixing

--- public/sketchB.js
+++ public/sketchB.js
@@ -1,26 +1,22 @@
+/*globals createCanvas background fill ellipse*/
 var y = 60;
 var d=80;
 function setup() {
   createCanvas(480,120);
 }
 function draw() {
-<<<<<<< HEAD
-<<<<<<< master
+
 background(0,26,51);
-=======
 background(0,0,255);
 fill(255); // WHITE
 // fill(255,0,0); // RED
->>>>>>> 75333e0 a few colour changes
-=======
 background(0,0,255);
 fill(255); // WHITE
 // fill(255,0,0); // RED
->>>>>>> 75333e0c6fabad65135487cce1a59730edd7d8e4
 ellipse(75,y,d,d);
 fill(255,255,0); //YELLOW
 // fill(0); // BLACK
 ellipse(175,y,d,d);
-fill(0,255,0) //GREEN
+fill(0,255,0); //GREEN
 ellipse(275,y,d,d);
 }
