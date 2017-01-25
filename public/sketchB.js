f74b69f47fb6a70f9512e93a2efe3043a33fccc0
JAMES SANDERS <jsanders@ca.ibm.com>
2017-01-25 10:46:20 -0600

    done on remote

--- public/sketchB.js
+++ public/sketchB.js
@@ -4,8 +4,17 @@ function setup() {
   createCanvas(480,120);
 }
 function draw() {
+<<<<<<< master
 background(0,26,51);
+=======
+background(0,0,255);
+fill(255); // WHITE
+// fill(255,0,0); // RED
+>>>>>>> 75333e0 a few colour changes
 ellipse(75,y,d,d);
+fill(255,255,0); //YELLOW
+// fill(0); // BLACK
 ellipse(175,y,d,d);
+fill(0,255,0) //GREEN
 ellipse(275,y,d,d);
 }
